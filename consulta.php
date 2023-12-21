8456aeb04f1ab0c3554a3c802ce380c5aec9c3eb
Leonardo Teodoro <38961152+imponateado@users.noreply.github.com>
2023-12-21 16:20:52 -0300

    modifications in columns
    now user can see entire data of a client and how many days since last bought

--- consulta.php
+++ consulta.php
@@ -25,27 +25,38 @@ if ($result->num_rows > 0) {
     echo "<br>";
     echo "<table border=1>";
         echo "<tr>";
-            echo "<td>Cliente</td>";
-            echo "<td>CPF/CNPJ</td>";
-            echo "<td>Endereco</td>";
-            echo "<td>Cidade</td>";
-            echo "<td>Telefone</td>";
-            echo "<td>Email</td>";
-            echo "<td>Data da última compra</td>";
-            echo "<td>Valor já comprado</td>";
-            echo "<td>Dias desde última compra</td>";
+            echo "<td>Cliente</td>"; // 1
+            echo "<td>CPF/CNPJ</td>"; //2
+            echo "<td>Endereco</td>"; //3
+            echo "<td>Cidade</td>"; //4
+            echo "<td>Telefone</td>"; //5
+            echo "<td>Email</td>"; //6
+            echo "<td>Data da última compra</td>"; //7
+            echo "<td>Valor já comprado</td>"; //8
+            echo "<td>Dias desde última compra</td>"; //9
         echo "</tr>";
         while($row = $result->fetch_assoc()) {
         echo "<tr>";
-            echo "<td>" . $row["nome"] . "</td>";
-            echo "<td>" . $row["cpfCNPJ"] . "</td>";
-            echo "<td>" . $row["endereco"] . "</td>";
-            echo "<td>" . $row["cidade"] . "</td>";
-            echo "<td>" . $row["telefone"] . "</td>";
-            echo "<td>" . $row["e-mail"] . "</td>";
-            echo "<td> REPLACEHERE </td>";
-            echo "<td>R$ " . $row["valorJaComprado"] . "</td>";
-            echo "<td> REPLACEHERE</td>";
+            echo "<td>" . $row["nome"] . "</td>"; //1
+            echo "<td>" . $row["cpfCNPJ"] . "</td>"; //2
+            echo "<td>" . $row["endereco"] . "</td>"; //3
+            echo "<td>" . $row["cidade"] . "</td>"; //4
+            echo "<td>" . $row["telefone"] . "</td>"; //5
+            echo "<td>" . $row["e-mail"] . "</td>"; //6
+            echo "<td> " . (function($row) {
+                $data_excel = $row["data"];
+                $data_unix = ($data_excel - 25569) * 86400;
+                return date("d/m/Y", $data_unix);
+            })($row) ." </td>"; //7
+            echo "<td>R$ " . $row["ultCompra"] . "</td>"; //8
+            echo "<td> " . (function($row){
+                $excelTimestamp = $row["data"];
+                $excelStartDateUnix = strtotime('1900-01-01');
+                $unixTimestamp = ($excelTimestamp - 25569) * 86400;
+                $currentTimestamp = time();
+                $daysOfDifference = floor(($currentTimestamp - $unixTimestamp) / 86400);
+                return $daysOfDifference;
+            })($row) . "</td>"; //9
         echo "</tr>";
         }
     echo "</table>";
